c58da0c975399b4ec3a90cf798c03506afc1abf0
FasterObnova <iscander9959@gmail.com>
2022-07-21 12:25:27 +0300

    added Task0

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+public class Task4 {
+    public static void main(String[] args) {
+
+        int[] aoi = new int[10];
+
+        for(int current : aoi) {
+            current = 1;
+
+            System.out.println(current);
+        }
+
+    }
+}
